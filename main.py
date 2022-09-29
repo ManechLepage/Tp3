da76a874214182dad37623fc1d41b76740db135b
ManechLepage <113209819+ManechLepage@users.noreply.github.com>
2022-09-29 10:41:09 -0400

    Rajoute le debut du combat

--- main.py
+++ main.py
@@ -3,21 +3,30 @@ import random
 life_score = 20
 monster_count = 0
 
-def generateFight(difficulty):
-    monster_count += 1
-    print(f"")
+
+def generate_fight(difficulty):
     number_rolled = random.randint(1, 6)
+    print("=" * 10)
     print(f"Lancé de dé : {number_rolled}")
-def generateNewFight():
+    print("=" * 10)
+
+
+def generate_new_fight():
     pass
+
+
 def settings():
     pass
+
+
 def quit():
     pass
+
+
 def choice(string):
     isChoice = False
     difficulty = random.randint(1, 6)
-    while not isChoice :
+    while not isChoice:
         choice = input(string)
         try:
             choice = int(choice)
@@ -26,16 +35,21 @@ def choice(string):
             print("Erreur. Veuillez choisir un nombre.")
 
     if choice == 1:
-        generateFight(difficulty)
+        monster_count += 1
+        generate_fight(difficulty)
     elif choice == 2:
-        generateNewFight()
+        generate_new_fight()
     elif choice == 3:
         settings()
     elif choice == 4:
         quit()
 
+
 def main():
-    choice("1: Combattre le monstre \n2: Fuir le combat et aller a une nouvelle porte\n3: Afficher les regles du jeu \n4: Quitter la partie")
+    difficulty = random.randint(1, 5)
+    choice(f"1: Combattre le monstre \n2: Fuir le combat et aller a une nouvelle porte\n3: Afficher les regles du jeu \n4: Quitter la partie\nDifficulté du monstre : {difficulty}\nChoix :  ")
+
+
 
 if __name__ == "__main__":
     main()
